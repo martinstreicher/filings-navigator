7ae3f4428b6cc21a855c7196588b855fad2e6040
Martin Streicher, Flywheel, Gadget, and Sprocket Consulting <martin.streicher@gadget.consulting>
2023-08-07 15:38:36 -0400

    wip

--- app/interactors/import_990_interactor.rb
+++ app/interactors/import_990_interactor.rb
@@ -5,5 +5,24 @@ class Import990Interactor < BaseInteractor
     result = ParseXmlInteractor.call(xml_file_url: xml_file_url).result
     raise result.errors unless result.success?
 
+    filer  = Filer.new result.filer_attributes
+    filing = Filing.new result.filing_attributes.merge(filer: filer)
+
+    awards = result.awards.map do |award|
+      recipient = Recipient.new award.recipient_attributes
+
+      GrantAward.new(
+        award.award_attributes
+          .merge(recipient: recipient)
+          .merge(filing: filing)
+        )
+    end
+
+    GrantAward.import(
+      awards,
+      validate: true,
+      validate_uniqueness: true,
+      recursive: true
+    )
   end
 end
